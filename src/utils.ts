3bdbcc2383a3f0d5c93c2dc5ea86c968d07a29ad
omniroot <omnirootofc@gmail.com>
2025-12-09 14:39:37 +0300

    REmove shit

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,4 @@
-export function installYay() {
-	const isAlreadyInstalled = !!exec("pacman -Qq | grep yay-bin");
-	if (isAlreadyInstalled) {
-		console.log("Yay is already installed!");
-		return;
-	}
 
-	const commands = [
-		"rm -rf /tmp/yay-bin",
-		"git clone https://aur.archlinux.org/yay-bin /tmp/yay-bin",
-		"cd /tmp/yay-bin",
-		"makepkg -sric --noconfirm --needed -D /tmp/yay-bin/",
-		"rm -rf /tmp/yay-bin",
-	];
-	commands.forEach((c) => {
-		const output = exec(c);
-		console.log(output);
-	});
-}
 
 export function installNextdns() {
 	const which = exec("which nextdns");
